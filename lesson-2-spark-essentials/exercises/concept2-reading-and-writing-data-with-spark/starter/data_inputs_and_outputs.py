94d85c6b20aa5d08e724cd7b9b5e513182d3f25a
Matthias Friebel <matthias.friebel@now-gmbh.de>
2023-12-08 17:01:12 +0100

    #Filled in excercise

--- lesson-2-spark-essentials/exercises/concept2-reading-and-writing-data-with-spark/starter/data_inputs_and_outputs.py
+++ lesson-2-spark-essentials/exercises/concept2-reading-and-writing-data-with-spark/starter/data_inputs_and_outputs.py
@@ -1,3 +1,10 @@
+###
+# You might have noticed this code in the screencast.
+#
+import findspark
+findspark.init('/opt/homebrew/Cellar/apache-spark/3.5.0/libexec')
+#
+
 import pyspark
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
@@ -7,3 +14,20 @@ spark = SparkSession \
     .appName("Our first Python Spark SQL example") \
     .getOrCreate()
 
+## READ DATA
+
+path = "../../data/sparkify_log_small.json"
+user_log = spark.read.json(path)
+user_log.printSchema()
+user_log.describe()
+user_log.show(n=1)
+user_log.take(5)
+
+## WRITE DATA
+out_path = "../../data/sparkify_log_small.csv"
+user_log.write.save(out_path, format="csv", header=True)
+user_log_2 = spark.read.csv(out_path, header=True)
+user_log_2.printSchema()
+user_log_2.take(2)
+user_log_2.select("userID").show()
+user_log_2.take(1)
