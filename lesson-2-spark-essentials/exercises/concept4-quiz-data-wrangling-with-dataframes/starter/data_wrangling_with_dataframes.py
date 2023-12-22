7e1eded10efd342b583e7416db92bd92aad747fe
Matthias Friebel <matthias.friebel@now-gmbh.de>
2023-12-22 12:40:47 +0100

    #CHANGES: filled in exercise

--- lesson-2-spark-essentials/exercises/concept4-quiz-data-wrangling-with-dataframes/starter/data_wrangling_with_dataframes.py
+++ lesson-2-spark-essentials/exercises/concept4-quiz-data-wrangling-with-dataframes/starter/data_wrangling_with_dataframes.py
@@ -1,6 +1,14 @@
 # # Data Wrangling with DataFrames Coding Quiz
+import findspark
+findspark.init('/opt/homebrew/Cellar/apache-spark/3.5.0/libexec')
 
 from pyspark.sql import SparkSession
+import pandas as pd
+
+sc = SparkSession.builder.appName("Sparkify").getOrCreate()
+
+path = "lesson-2-spark-essentials/exercises/data/sparkify_log_small.json"
+user_log = sc.read.json(path)
 
 # TODOS: 
 # 1) import any other libraries you might need
@@ -13,8 +21,10 @@ from pyspark.sql import SparkSession
 # 
 # Which page did user id "" (empty string) NOT visit?
 
-# TODO: write your code to answer question 1
+visited_pages = user_log.select("page").where(user_log.userId == "").dropDuplicates()
+distinct_pages = user_log.select("page").distinct()
 
+distinct_pages[~distinct_pages.page.isin(visited_pages.page)].show()
 
 # # Question 2 - Reflect
 # 
